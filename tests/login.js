9ebfd1946a7fc7e089bdca1b4a800ff3ba6b96b6
Alexei Vinogradov <alexei@vinogradoff.de>
2014-03-12 18:50:14 +0100

    screenshots folder + screenshotting  for login-test added

--- tests/login.js
+++ tests/login.js
@@ -1,30 +1,32 @@
-function scr(browser){
-   browser.saveScreenshot("1.jpg");
-}
+function _s(){
+  now=new Date();
+  folder=""+now.getFullYear()+(now.getMonth()+1)+now.getDate();
+  filename=""+now.getHours()+now.getMinutes()+now.getSeconds();
+  this.saveScreenshot("screenshots/"+folder+"-"+filename+".png");
+};
 module.exports = {
   "Opt-in for ebay 3.0" : function (browser) {
     browser
-      .url('http://cgi1.ebay.de/ws/eBayISAPI.dll?SetGuidforExpt&parm=AQABAAAAMJlpPqZCuMUmHrsVN4FqdGNYnC-BZVRCj0cmRmGSu2AQNj7CMgVANpnSie5rtZx77w**')
+      .url('http://cgi1.ebay.de/ws/eBayISAPI.dll?SetGuidforExpt&parm=AQABAAAAMJlpPqZCuMUmHrsVN4FqdGNYnC-BZVRCj0cmRmGSu2AQNj7CMgVANpnSie5rtZx77w**',_s)
   },
   "Login at eBay.de" : function (browser) {
     browser
-      .url("http://www.ebay.de")
+      .url("http://www.ebay.de",_s)
       .waitForElementVisible('body', 1000)
       .assert.containsText('#gh-ug', 'Einloggen')
-      .click('#gh-ug a',scr)
+      .click('#gh-ug a',_s)
       .waitForElementVisible('body', 1000)
       .assert.containsText('#mainCnt div div h2', 'Einloggen')
       .setValue('#userid', '2014detestu')
       .setValue('#pass','EGu-TFw-M7C-W9g')
-      .click('#sgnBt')
+      .click('#sgnBt',_s)
       .waitForElementVisible('body', 1000)
       .assert.containsText('#gh-ug', '2014detestu')
-      .click('#gh-la')
+      .click('#gh-la',_s)
   },
   "Verify ebay 3.0" : function (browser) {
     browser
       .verify.cssClassPresent('#top div ul li:nth-child(2)','today')
-      .pause(10000)
       .end();
   }
   

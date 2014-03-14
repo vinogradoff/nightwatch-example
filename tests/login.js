349f243947ca7fac0319def69f365918af226f58
Alexei Vinogradov <alexei@vinogradoff.de>
2014-03-14 15:17:55 +0100

    credentials externalized

--- tests/login.js
+++ tests/login.js
@@ -1,15 +1,29 @@
+var mkdirp = require('mkdirp');
+var startMoment = require('moment');
+var folder='login-'+startMoment().format('YYYYDDMM-HHmm');
+mkdirp('screenshots/'+folder, function(err) { 
+    // path was created unless there was error
+});
+
 function _s(){
   var moment = require('moment');
-  folder=moment().format('YYYYDDMM'); 
   filename=moment().format('HHmmssS');
-  this.saveScreenshot("screenshots/"+folder+"-"+filename+".png");
+  this.saveScreenshot("screenshots/"+folder+"/"+filename+".png");
 };
+
 module.exports = {
   "Opt-in for ebay 3.0" : function (browser) {
     browser
       .url('http://cgi1.ebay.de/ws/eBayISAPI.dll?SetGuidforExpt&parm=AQABAAAAMJlpPqZCuMUmHrsVN4FqdGNYnC-BZVRCj0cmRmGSu2AQNj7CMgVANpnSie5rtZx77w**',_s)
   },
   "Login at eBay.de" : function (browser) {
+    try {
+      var ebaycredentials = require('./ebaycredentials.json');  
+    } catch (err) {
+      console.error('Couldn\'t load the ebay credentials file. Please ensure that ' +
+        'you have the fbcredentials.json in the same folder as the test.')
+      process.exit();      
+    }
     browser
       .url("http://www.ebay.de",_s)
       .waitForElementVisible('body', 1000)

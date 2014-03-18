e52848b13b0427d6d9be59179115298b6a94729e
Alexei Vinogradov <alexei@vinogradoff.de>
2014-03-18 15:55:05 +0100

    better screenshots folder names

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ nightwatch-example
 
 requires: nodeJS (download & install from http://nodejs.org/)
 
+includes node modules: nightwatch, moment, mkdirp 
+
 selenium-server is included as selenium-server-standalone-2.40.0.jar,<br>
 you can replace it with actual version from http://seleniumhq.org),<br>
 you will need to update setting.json then.<br>
@@ -14,6 +16,13 @@ starting tests:<br>
 all tests:          ./nightwatch<br>
 some tests:         ./nightwatch -t tests/&lt;test1&gt;<br>
 some test group:    ./nightwatch -g tests/&lt;test1&gt;,tests/&lt;test2&gt;...<br>
-all tests, expecpt: ./nightwatch -s tests/&lt;test1&gt;,tests/&lt;test2&gt;...<br>
+all tests, except: 	./nightwatch -s tests/&lt;test1&gt;,tests/&lt;test2&gt;...<br>
+
+changing enviroments:
+
+production:          ./nightwatch<br>
+pre-prod:			 ./nightwatch -e preprod<br>	
+staging:			 ./nightwatch -e staging<br>
+
 
 
--- tests/short.js
+++ tests/short.js
@@ -1,20 +1,32 @@
+/** 
+*	screenshot handling
+*/
+var screenshotPath='screenshots'; 
+
+var path=require('path');
 var mkdirp = require('mkdirp');
 var startMoment = require('moment');
-var folder='login-'+startMoment().format('YYYYDDMM-HHmm');
-mkdirp('screenshots/'+folder, function(err) { 
+var folder=path.basename(__filename)+'-'+startMoment().format('YYYYDDMM-HHmm');
+mkdirp(screenshotPath+'/'+folder, function(err) { 
     // path was created unless there was error
 });
 
 function _s(){
   var moment = require('moment');
   filename=moment().format('HHmmssS');
-  this.saveScreenshot("screenshots/"+folder+"/"+filename+".png");
+  this.saveScreenshot(screenshotPath+'/'+folder+"/"+filename+".png");
 };
+
+/** 
+*	test code
+*/
+
 
 module.exports = {
   "Show google.de" : function (browser) {
      browser
       .url("http://www.google.de",_s)
+      .assert.containsText("tralala", "55")
       .end();
   }
 };

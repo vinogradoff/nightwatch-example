37290b821d99a93184c2e326a16edf040711c2f6
Alexei Vinogradov <alexei@vinogradoff.de>
2014-03-18 18:03:56 +0100

    settings refactored

---
+++ tests/collections.js
@@ -0,0 +1,36 @@
+//include screenshot _s function
+var fs = require('fs');
+eval(fs.readFileSync('screenshot_helper.js')+'');
+
+module.exports = {
+ "Login at eBay.de" : function (browser) {
+     browser
+      .url(browser.launch_url,_s)
+      .waitForElementVisible('body', 1000)
+      .assert.containsText('#gh-ug', 'Einloggen')
+      .click('#gh-ug a',_s)
+      .waitForElementVisible('body', 1000)
+      .assert.containsText('#mainCnt div div h2', 'Einloggen')
+      .setValue('#userid', browser.options.ebaycredentials.username)
+      .setValue('#pass', browser.options.ebaycredentials.password)
+      .click('#sgnBt',_s)
+      .waitForElementVisible('body', 1000)
+      .assert.containsText('#gh-ug', 'Hallo,')
+      .click('#gh-la',_s)
+  },
+  "Verify ebay 3.0" : function (browser) {
+    browser
+      .verify.cssClassPresent('#top div ul li:nth-child(2)','today')
+      .end();
+  },
+  "Find collection" : function (browser) {
+  },
+  "Add item to new collection" : function (browser) {
+  },
+  "Show collection page" : function (browser) {
+  },
+  "Delete collection" : function (browser) {
+  },
+  "Logout" : function (browser) {
+  }
+};
